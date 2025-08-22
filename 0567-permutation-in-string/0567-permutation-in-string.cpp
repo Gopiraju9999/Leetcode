fc3260c25e350ccbfc8912d762b51b57916741e8
Gopiraju9999 <muvvagopiraju909@gmail.com>
2025-08-23 00:09:32 +0530

    Time: 3 ms (69.46%), Space: 10 MB (43.03%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    bool isfreqsame(int freq1[], int freq2[]){
-        for(int i = 0; i < 26; i++){
-            if(freq1[i] != freq2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
     bool checkInclusion(string s1, string s2) {
         if(s1.size() > s2.size()) return false;
 
-        int freq[26] = {0}; // s1 => i (pointer)
-        for(int i = 0; i<s1.size(); i++){
-            freq[s1[i] - 'a']++;
+       vector<int> s1count(26,0), s2count(26,0);
+
+       for(int i = 0; i < s1.size(); i++){
+        s1count[s1[i] - 'a']++;
+        s2count[s2[i] - 'a']++;
+       }
+        for(int i = 0; i< s2.size() - s1.size(); i++){
+            if(s1count == s2count) return true;
+            // update the window (move forward)
+            s2count[s2[i] - 'a']--;
+            s2count[s2[i + s1.size()] - 'a']++;
         }
-        int window_size = s1.size();
-        for(int i = 0; i<s2.size(); i++){
-            int window_idx = 0; // window starting point 
-            int idx = i; // window ending point 
-            int window_freq[26] = {0};
-            while(window_idx < window_size && idx < s2.size()){ // till goes end of s2 length
-                window_freq[s2[idx] - 'a']++; // increment the frequency
-                window_idx++; idx++; // always move the move
-            }
-            if(isfreqsame(freq, window_freq)) return true;
-        }
-        return false;
+        return s1count == s2count;
     }
 };
