ebca6d471288592d9fc353dca910089b8fe957d6
Gopiraju9999 <muvvagopiraju909@gmail.com>
2025-09-10 20:08:04 +0530

    Time: 3 ms (25.62%), Space: 34.8 MB (50.42%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) {
+       int i = 0, j = 0;
+       for(string cmd : commands){
+        if(cmd == "UP") i -= 1;
+        else if(cmd == "DOWN") i += 1;
+        else if(cmd == "LEFT") j -= 1;
+        else if(cmd == "RIGHT") j +=1;
+       } 
+       return (i*n) + j; // selects the current stopped position in 2D matrix
+    }
+};
