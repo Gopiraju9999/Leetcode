7496d79ddcd6745e849fe7767fa82960dd769910
Gopiraju9999 <muvvagopiraju909@gmail.com>
2025-10-12 10:30:51 +0530

    Time: 0 ms (100%), Space: 15 MB (60.86%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -25,10 +25,10 @@ public:
                 q.pop();
                 level.push_back(node->val);
 
-                if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
+                if(node->left) q.push(node->left);
             }
-            result.push_back(level.back());
+            result.push_back(level.front());
         }
         return result;
     }
