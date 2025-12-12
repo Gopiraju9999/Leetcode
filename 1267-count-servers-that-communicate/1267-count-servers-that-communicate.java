0acc00e9dfdc98c1921f2483a2150223096e731d
Gopiraju9999 <muvvagopiraju909@gmail.com>
2025-12-12 22:03:48 +0530

    Time: 2 ms (100%), Space: 55.4 MB (24.43%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -17,7 +17,6 @@ class Solution {
             }
         }
 
-        int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1 && (row[i] == 1 && column[j] == 1)){
