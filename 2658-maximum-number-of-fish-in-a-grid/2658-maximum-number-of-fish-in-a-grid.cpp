8b4dbc2e3adb7a639289584927b9532415d833b0
Gopiraju9999 <muvvagopiraju909@gmail.com>
2025-12-22 21:51:08 +0530

    Time: 0 ms (100%), Space: 94 MB (86.8%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int DFS(vector<vector<int>>& grid, int i, int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] == 0) 
+    int DFS(vector<vector<int>>& grid, int i, int j, int m, int n){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] == 0) 
             return 0;
 
         int fish = grid[i][j]; // Stores the fishes at the cell 
         grid[i][j] = 0;          // Mark as visited..
 
-        fish += DFS(grid, i-1, j);
-        fish += DFS(grid, i+1, j);
-        fish += DFS(grid, i, j-1);
-        fish += DFS(grid, i, j+1);
+        fish += DFS(grid, i-1, j, m, n);
+        fish += DFS(grid, i+1, j, m, n);
+        fish += DFS(grid, i, j-1, m ,n);
+        fish += DFS(grid, i, j+1, m, n);
 
         return fish;
     }
@@ -22,7 +22,7 @@ public:
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] > 0){
-                    fishes = DFS(grid, i, j); // Way to call DFS & stores the value..
+                    fishes = DFS(grid, i, j, m, n); // Way to call DFS & stores the value..
                     maxi = max(maxi, fishes);
                 }
             }
