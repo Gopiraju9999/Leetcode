d9b82f36809e100216ceda7ef5b19cecdcc54119
Gopiraju9999 <muvvagopiraju909@gmail.com>
2025-12-22 23:05:13 +0530

    Time: 33 ms (58.06%), Space: 78.5 MB (93.09%) - LeetHub

--- 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.cpp
+++ 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>& grid, int i, int j, int m, int n, long long& sum){
-        if(i<0 || i>= m || j<0 || j>= n || grid[i][j] == 0) return;
+    long long DFS(vector<vector<int>>& grid, int i, int j, int m, int n){
+        if(i<0 || i>= m || j<0 || j>= n || grid[i][j] == 0) return 0;
 
-        sum += grid[i][j];  // Storing the value at current cell
+        long long sum = grid[i][j];  // Storing the value at current cell
         grid[i][j] = 0;       // Mark it as visited..
 
-        DFS(grid, i-1, j, m, n, sum);
-        DFS(grid, i+1, j, m, n, sum);
-        DFS(grid, i, j-1, m, n, sum);
-        DFS(grid, i, j+1, m, n, sum);
+        sum += DFS(grid, i-1, j, m, n);
+        sum += DFS(grid, i+1, j, m, n);
+        sum += DFS(grid, i, j-1, m, n);
+        sum += DFS(grid, i, j+1, m, n);
 
+        return sum;
     }
     int countIslands(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
         int count = 0;
+        long long sum = 0;
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] > 0){
-                    long long sum = 0;
-                    DFS(grid, i, j, m, n, sum);
+                   sum = DFS(grid, i, j, m, n);
 
                     if(sum % k == 0)
                         count++;
