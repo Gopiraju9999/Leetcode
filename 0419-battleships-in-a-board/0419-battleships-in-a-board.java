90dc5eb4e6cfeded63fc1a816cab57bf15bc6e4c
Gopiraju9999 <muvvagopiraju909@gmail.com>
2025-12-23 11:46:41 +0530

    Time: 1 ms (98.16%), Space: 45.6 MB (35.99%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,13 +1,13 @@
 class Solution {
-    private void DFS(char[][] board, int i, int j, int m, int n){
-        if(i<0 || i>= m || j<0 || j>=n || board[i][j] == '.') return;
+    private void DFS(char[][] board, int i, int j){
+        if(i<0 || i>= board.length || j<0 || j>=board[0].length || board[i][j] == '.') return;
 
         board[i][j] = '.'; // Mark it as visited cell..
 
-        DFS(board, i-1, j, m, n);
-        DFS(board, i+1, j, m, n);
-        DFS(board, i, j-1, m, n);
-        DFS(board, i, j+1, m, n);
+        DFS(board, i-1, j);
+        DFS(board, i+1, j);
+        DFS(board, i, j-1);
+        DFS(board, i, j+1);
     }
     public int countBattleships(char[][] board) {
         int m = board.length, n = board[0].length;
@@ -16,7 +16,7 @@ class Solution {
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(board[i][j] == 'X'){
-                    DFS(board, i, j, m, n);
+                    DFS(board, i, j);
                     count++;
                 }
             }
