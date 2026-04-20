b4be707efdcfc9128b7eee527a93f87e0941f34f
Gopiraju9999 <muvvagopiraju909@gmail.com>
2026-04-21 00:15:55 +0530

    Time: 18 ms (7.5%), Space: 46.9 MB (21.94%) - LeetHub

---
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long countVowels(String word) {
+        int n = word.length();
+        long count = 0;
+
+        for(int i = 0; i < n; i++){
+            char ch = word.charAt(i);
+            if(isVowel(ch)){
+                count += (long)(i + 1) * (n - i);
+            }
+        }
+        return count;
+    }
+    private boolean isVowel(char ch){
+        return "aeiouAEIOU".indexOf(ch) != -1;
+    }
+}
