636e326ec135e30d83bd67c15514b7632a538f6a
Gopiraju9999 <muvvagopiraju909@gmail.com>
2026-08-11 11:38:58 +0530

    Time: 4 ms (16.39%), Space: 43.8 MB (12.46%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,11 +1,11 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        StringBuilder ans = new StringBuilder();
+        String[] str = s.split(" ");
+        String result = "";
 
-        String[] str = s.split(" "); // Splits the string having space..
         for(int i = 0; i < k; i++){
-            ans.append(str[i]).append(" "); 
+            result += " " + str[i];
         }
-        return ans.toString().trim();  // way to ignore spaces at front & back
+        return result.trim();
     }
 }
