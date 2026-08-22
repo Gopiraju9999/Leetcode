0e5d5943fc80dd0eb8bf461d3e62656bac38cbb9
Gopiraju9999 <muvvagopiraju909@gmail.com>
2026-08-22 12:42:15 +0530

    Time: 46 ms (18.51%), Space: 46.4 MB (58.21%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,7 +1,11 @@
 class Solution {
-    private boolean contains(int[] map_S, int[] map_T){
+    private boolean contains(int[] s_count, int[] t_count){
+        // map_T is standard. Store the map_T characters frequency count into array..
+        // Now check one by one character, while moving the map_S array..
+        // It checks, if every character(index) in "map_T" array was greater than "map_S"
+        // Return false. Because, the respective character freq is more than map_T..
         for(int i = 0; i < 256; i++){
-            if(map_S[i] < map_T[i]){  // Way to check character by character in both maps..
+            if(s_count[i] < t_count[i]){  
                 return false;
             }
         }
@@ -9,26 +13,35 @@ class Solution {
     }
 
     public String minWindow(String s, String t) {
-        int[] map_S = new int[256]; // lowercase => "26",   uppercase => "52"
-        int[] map_T = new int[256]; // ASCII values => "256"
+        // Need 2 arrays to store the characters count at 0(1) space..
+        int[] s_count = new int[256]; // lower[26], upper[26], both[52], ASCII[256]
+        int[] t_count = new int[256];
 
-        for(char ch : t.toCharArray()){ // Storing characters into map
-            map_T[ch]++;
+        // String "t" characters are standard. So, store t characters..
+        for(char ch : t.toCharArray()){
+            t_count[ch]++;
         }
 
-        int i = 0, min_len = Integer.MAX_VALUE, minstart = 0;
+        // Remaining work will be, storing the string s characters one by one..
+        // Keep tracking any of the character in "s" < "t". Then, shrink the window..
+        int i = 0, min_length = Integer.MAX_VALUE, minstart = 0;
 
         for(int j = 0; j < s.length(); j++){
-            map_S[s.charAt(j)]++;
+            char ch = s.charAt(j);
+            s_count[ch]++;
 
-            while(contains(map_S, map_T)){
-                if(j-i+1 < min_len){
-                    min_len = j-i+1;
+            // It aloows if the character size is equal and checks for min length
+            while(contains(s_count, t_count)){
+                if(j-i+1 < min_length){
+                    min_length = j-i+1;
                     minstart = i;
                 }
-                map_S[s.charAt(i++)]--;
+                // It doesn't exist the characters, simpily shrink the window
+                s_count[s.charAt(i)]--;
+                i++;
             }
         }
-        return min_len == Integer.MAX_VALUE ? "" : s.substring(minstart, minstart+min_len);
+        return min_length == Integer.MAX_VALUE ? 
+        "" : s.substring(minstart, minstart+min_length);
     }
 }
