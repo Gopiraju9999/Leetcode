11f6509eb60675de1a6425f6f6497f88b567e1e6
Gopiraju9999 <muvvagopiraju909@gmail.com>
2026-08-22 23:13:20 +0530

    Time: 3 ms (96.02%), Space: 52.2 MB (36.08%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,20 +1,23 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+        // Sliding window approach
+        // window keep going forward, if element is "0" increment zerocount++
+        // Because, it's useful for considering "k" elements into window
         int n = nums.length;
-        int i = 0, maxi = 0, zero_count = 0;
+        int i = 0, maxi = Integer.MIN_VALUE, zero_count = 0;
 
         for(int j = 0; j < n; j++){
-            if(nums[j] == 0){
-                zero_count++;
-            }
-            // Here, the window has maximum zeros. Stop here and find the length
-            while(zero_count > k){ 
+            // zero found increment the zero count
+            if(nums[j] == 0) zero_count++;
+
+            // If the window has more than "k" zeroes. Shrink the window..
+            while(zero_count > k){
+                // Decrease the zerocount if the i pointer found "0"
                 if(nums[i] == 0){
                     zero_count--;
                 }
                 i++;
             }
-
             maxi = Math.max(maxi, j-i+1);
         }
         return maxi;
