df6c46341aa648499b44200ee604b1ad8ea63e3c
Gopiraju Muvva <muvvagopiraju909@gmail.com>
2026-08-29 11:32:59 +0530

    Time: 6 ms (87.28%), Space: 45.5 MB (15.5%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -5,31 +5,31 @@ class Solution {
         for(String token : tokens){
             // Checking my current element is pointing to operator or not??
             // if it is simply pop 2 elements and perform the respective operation
-            if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
-                int b = st.pop();
-                int a = st.pop();
-                int result = 0;
-                
-                // The operations which will be exist..
+            // Push back the performed result into stack
+
                 if(token.equals("+")){
-                    result = a + b;
+                    int b = st.pop();
+                    int a = st.pop();
+                    st.push(a+b);
                 }else if(token.equals("-")){
-                    result = a - b;
+                    int b = st.pop();
+                    int a = st.pop();
+                    st.push(a-b);
                 }else if(token.equals("*")){
-                    result = a * b;
-                }else{
-                    result = a / b;
+                    int b = st.pop();
+                    int a = st.pop();
+                    st.push(a*b);
+                }else if(token.equals("/")){
+                    int b = st.pop();
+                    int a = st.pop();
+                    st.push(a/b);
                 }
-                st.push(result);
-            }
-            // Here is the condition only for the numbers
-            // push into it & convert into interger value
-            else{
-                st.push(Integer.parseInt(token));
-            }
+                // Here we get the only numbers. so, simply push into stack..
+                else{
+                    st.push(Integer.parseInt(token));
+                }
         }
-        // At the end, we have the answer only at the top
-        // Then return it..
+        // At the end, the top of stack will be answer. so, simply return it..
         return st.pop();
     }
 }
