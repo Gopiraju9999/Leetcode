f04fb2d98a9906501a394acbe164f7c1bada442b
Gopiraju Muvva <muvvagopiraju909@gmail.com>
2026-09-01 00:01:32 +0530

    Time: 26 ms (14.27%), Space: 48.4 MB (10.99%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,18 +1,27 @@
 class Solution {
+    
+    // For every Binary Search on Answers type of problems. We should follow 3 steps:
+    // Step1: Finding mini & maxi for doing Binary Search operations..
+    // Step2: It's heart of problem, doing calulations according to problem. Run a loop in the given array and do calculations "with the presence of mind".
+    // Step3: It's nothing.. Take range from mini to maxi in the array and perform operation
+    // Brute force : Linear search for answers 0(n), optimal : Binary search for answers 0(logn)
+    // Be Careful with supportive functions..
+
     private int FindMax(int[] piles){
-        int max = 0;
+        int maxi = 0;
         for(int pile : piles){
-            if(pile > max)
-                max = pile;
+            if(pile > maxi){
+                maxi = pile;
+            }
         }
-        return max;
+        return maxi;
     }
 
     private boolean canEat(int[] piles, int h, int k){
         long hours = 0;
 
         for(int i = 0; i < piles.length; i++){
-            hours += Math.ceil((double)piles[i] / k);
+            hours += Math.ceil((double) piles[i] / k);
 
             if(hours > h) return false;
         }
@@ -20,16 +29,16 @@ class Solution {
     }
     
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1, high = FindMax(piles), result = high;
+        int low = 1, high = FindMax(piles), result = high; // Initially, took higher for safety
 
         while(low <= high){
             int mid = low + (high - low) / 2;
 
             if(canEat(piles, h, mid)){
                 result = mid;
-                high = mid-1;
+                high = mid - 1;
             }else{
-                low = mid+1;
+                low = mid + 1;
             }
         }
         return result;
