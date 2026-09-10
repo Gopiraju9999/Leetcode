b1785ba15a037fefafd08e99c02791c058135adf
Gopiraju Muvva <muvvagopiraju909@gmail.com>
2026-09-10 23:20:07 +0530

    Time: 2 ms (75.83%), Space: 47.8 MB (56.64%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,24 +1,34 @@
 class Solution {
     public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
+        int m = board.length;    // "m" for columns
+        int n = board[0].length; // "n" for rows
+        
+        // Our aim is to make cells as "X" except boundary 0's
+        // Check boundaries has "0" and also make connected cells with "0" as "X"
+        // Initially, boundary "0" convert it into "#". 
+        // Then, through out matrix i,j == 0 convert into "X" & "#" convert into "0"
 
+        // Check the first & last columns
         for(int i = 0; i < m; i++){
-            if(board[i][0] == 'O')
+            if(board[i][0] == 'O'){
                 DFS(board, i, 0, m, n);
-            if(board[i][n-1] == 'O')
+            }
+            if(board[i][n-1] == 'O'){
                 DFS(board, i, n-1, m, n);
+            }
         }
 
-        // Checking the first and last columns..
+        // Check the first & last rows
         for(int j = 0; j < n; j++){
-            if(board[0][j] == 'O')
+            if(board[0][j] == 'O'){
                 DFS(board, 0, j, m, n);
-            if(board[m-1][j] == 'O')
+            }
+            if(board[m-1][j] == 'O'){
                 DFS(board, m-1, j, m, n);
+            }
         }
-
-        // Checking the first and last Rows..
+        
+        // Original matrix for making changes in it..
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(board[i][j] == 'O')
@@ -29,10 +39,12 @@ class Solution {
         }
     }
     private void DFS(char[][] board, int i, int j, int m, int n){
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j] != 'O') return;
+        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O') return;
 
-        board[i][j] = '#';
+        // Means, it's a boundary "o" cell mark it as "#"
+        board[i][j] = '#'; 
 
+        // Calling DFS calls for 4 possible moves (up, down, left, right)
         DFS(board, i-1, j, m, n);
         DFS(board, i+1, j, m, n);
         DFS(board, i, j-1, m, n);
