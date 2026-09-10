25b3a816d2f196fa71293b1cc13d6734155492b5
Gopiraju Muvva <muvvagopiraju909@gmail.com>
2026-09-11 01:09:16 +0530

    Time: 3 ms (100%), Space: 47.2 MB (12.93%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
@@ -1,30 +1,31 @@
 class Solution {
-    private int DFS(int[][] grid, int i, int j, int m, int n){
-        if(i<0 || i >=m || j<0 || j>=n || grid[i][j] == 0) return 0;
-
-        int fish = grid[i][j];   // Store the value
-        grid[i][j] = 0;            // Mark as visited..
-
-        fish += DFS(grid, i-1, j, m, n);
-        fish += DFS(grid, i+1, j, m, n);
-        fish += DFS(grid, i, j-1, m, n);
-        fish += DFS(grid, i, j+1, m, n);
-
-        return fish;
-    }
     public int findMaxFish(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int fishes = 0, maxi = 0;
+        int maxi_fishes = 0;
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] > 0){
-                    fishes = DFS(grid, i, j, m, n); // Way to run DFS and stores values with DFS
-                    maxi = Math.max(maxi, fishes);
+                    int fishes = DFS(grid, i, j, m, n);
+                    maxi_fishes = Math.max(maxi_fishes, fishes);
                 }
             }
         }
-        return maxi;
+        return maxi_fishes;
+    }
+
+    private int DFS(int[][] grid, int i, int j, int m, int n){
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) return 0;
+
+        int fishes = grid[i][j];  // Take that cell fishes as initial fishes
+        grid[i][j] = 0;           // And make the cell as "0" for good calculation
+
+        fishes += DFS(grid, i+1, j, m, n);
+        fishes += DFS(grid, i-1, j, m, n);
+        fishes += DFS(grid, i, j+1, m, n);
+        fishes += DFS(grid, i, j-1, m, n);
+
+        return fishes;
     }
 }
