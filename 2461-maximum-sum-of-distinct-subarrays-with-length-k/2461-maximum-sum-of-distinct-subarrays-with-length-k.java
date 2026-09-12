9ff9c2a5f77c479ab0b972411c77ff7993dd068c
Gopiraju Muvva <muvvagopiraju909@gmail.com>
2026-09-12 11:50:01 +0530

    Time: 32 ms (93.13%), Space: 98.9 MB (62.2%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -4,19 +4,20 @@ class Solution {
         Set<Integer>st = new HashSet<>();
         long sum = 0, maxi_sum = 0;
 
-        if(n < k) return 0;
-
-        int i = 0;
+        int i = 0; 
         for(int j = 0; j < n; j++){
 
+            // Check the element present in set or not??
             while(st.contains(nums[j])){
                 st.remove(nums[i]);
                 sum -= nums[i];
                 i++;
             }
+            // element is not present in set. Then, add into set
             st.add(nums[j]);
             sum += nums[j];
 
+            // Now, my current window size is "k-sized". Calculate the maximum & shrink window
             if(j-i+1 == k){
                 maxi_sum = Math.max(maxi_sum, sum);
                 st.remove(nums[i]);
