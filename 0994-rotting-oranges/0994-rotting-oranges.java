f5477f2c5b761bcd9d1b4f2b3f24121017a9374e
Gopiraju Muvva <muvvagopiraju909@gmail.com>
2026-09-16 00:48:44 +0530

    Time: 2 ms (87.69%), Space: 43.9 MB (91.42%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,46 +1,52 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length;        // for columns
+        int n = grid[0].length;     // for rows
+        Queue<int[]>q = new LinkedList<>();
         int fresh_count = 0;
-        Queue<int[]>que = new LinkedList<>();
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 2){
-                    que.offer(new int[]{i, j});
-                } else if(grid[i][j] == 1){
+                    q.offer(new int[]{i, j});
+                }else if(grid[i][j] == 1){
                     fresh_count++;
                 }
             }
         }
-
         if(fresh_count == 0) return 0;
         int minutes = 0;
-        int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        int[][] directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-        while(!que.isEmpty()){
-            int size = que.size();
-            while(size-- > 0){
-                int[] curr = que.poll(); // poll => Takes the front one & pop out
-                int i = curr[0];
-                int j = curr[1];
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            for(int k = 0; k < size; k++){
+                int[] node = q.poll();
+                int i = node[0];
+                int j = node[1];
 
                 for(int[] dir : directions){
-                    // Choose there Neighbourss..
                     int new_i = i + dir[0];
                     int new_j = j + dir[1];
 
-                    if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && 
-                    grid[new_i][new_j] == 1){
+                    if(isValid(new_i, new_j, m, n) && grid[new_i][new_j] == 1){
+                        // Make rotten
                         grid[new_i][new_j] = 2;
+                        // Push their adjacent 4 direction cells having "1"
+                        q.offer(new int[]{new_i, new_j});
+                        // And decrement the freshcount. Because,fresh became rotten
                         fresh_count--;
-                        que.offer(new int[]{new_i, new_j});
                     }
                 }
             }
             minutes++;
         }
-        return fresh_count == 0 ? (minutes - 1) : -1;
+        // At the end, any of fresh orange is remained. 
+        //It means their is a orange lies btw empty cells & couldn't become rotten
+        return (fresh_count == 0) ? (minutes - 1) : -1;
+    }
+    private boolean isValid(int i, int j, int m, int n){
+        return (i >= 0 && i < m && j >= 0 && j < n);
     }
 }
